7c99cf9e567f51b550b5b142678b820f8cdaca37
Ezequiel Rubio <eze.rubio@gmail.com>
2015-02-27 21:28:06 +0100

    Added tests

--- public/js/tests/client/spec/appContextSpec.js
+++ public/js/tests/client/spec/appContextSpec.js
@@ -1,7 +1,7 @@
 define(['plugins/app-context'], function(app) {
 	'use strict';
 
-	describe('Csv Parser', function() {
+	describe('App context', function() {
 
 		it('Should return app context', function(done) {
 			expect(app.version).toEqual('0.1.0');
