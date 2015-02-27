c232b76acb60f6aab289131f85400202b0b62c9b
Ezequiel Rubio <eze.rubio@gmail.com>
2015-02-27 21:27:04 +0100

    Map module and plugin

--- public/js/modules/map.js
+++ public/js/modules/map.js
@@ -1,9 +1,59 @@
 (function() {
 	'use strict';
 
-	define(['jquery'], function($) {
-		
+	define(['jquery', 'plugins/google-maps','plugins/events-manager'], function($, gmaps, eManager) {
+		var map, markers = [];
+
+
+		/**
+		* Function that init google maps
+		**/
+		function initMaps() {
+			var mapOptions = {
+				center: new gmaps.LatLng(0, 0),
+				zoom: 2
+			};
+			map = new gmaps.Map(document.getElementById('map'), mapOptions);
+		}
+
+		/**
+		* Function that add markers and center map to view all of them
+		* @param {Object} data -> markers data
+		**/
+		function addMapMarkers(data) {
+			var i, l, latlng, title, currentFounder,
+				bounds = new gmaps.LatLngBounds();
+			
+			for(i=0,l=data.length; i<l; i++) {
+				currentFounder = data[i];
+				latlng = new gmaps.LatLng(currentFounder.garageLatitude, currentFounder.garageLongitude);
+				title = currentFounder.companyName;
+				markers.push(new gmaps.Marker({
+					position: latlng,
+					map: map,
+					title: title
+				}));
+				bounds.extend(latlng);
+			}
+			map.fitBounds(bounds);
+			
+		}
+
+		/**
+		* Function that start listening events for the module
+		**/
+		function bindEvents() {
+			eManager.on('addedFounders', function(data) {
+				addMapMarkers(data);
+			});
+		}
+
+		/**
+		* Function that init maps module
+		**/
 		function init() {
+			initMaps();
+			bindEvents();
 			console.log('map module initialized!');
 		}
 
---
+++ public/js/plugins/google-maps.js
@@ -0,0 +1,4 @@
+define(['async!http://maps.google.com/maps/api/js?sensor=false'],function() {
+    //plugin for google maps
+    return google.maps;
+});
